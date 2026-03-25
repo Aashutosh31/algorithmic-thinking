9618bb0481216e4c110605c0fc87c9bf8b1fb819
Aashutosh Bairagi <aashutosh.bairagi05@gmail.com>
2026-03-25 15:21:42 +0530

    basic function

---
+++ cpp-basics/basics/functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Function declaration
+int  printName(){
+    cout << "Aashutosh" ;
+    return 32;
+}
+
+int main () {
+int val = printName();
+cout << endl <<"val = " << val << endl;
+}

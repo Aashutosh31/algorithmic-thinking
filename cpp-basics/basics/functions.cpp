0abeb73d093418fa162ed31406fe338cb3b582f0
Aashutosh Bairagi <aashutosh.bairagi05@gmail.com>
2026-03-25 16:03:06 +0530

    1 to n in functions

--- cpp-basics/basics/functions.cpp
+++ cpp-basics/basics/functions.cpp
@@ -16,3 +16,23 @@ int main () {
     cout << printMin(24,12);
 }
 */
+
+//sum 1 to n 
+int sum(int n ){
+ int total = 0;
+ for (int i = 1 ; i <= n ; i++){
+    total += i ;
+
+ }   
+  return total;
+}
+
+// O(1) complexity
+int bigO1(int n){
+    return n * (n+1)/2;
+}
+
+int main () {
+    cout << bigO1(9) << endl;
+    cout << sum(9);
+}
